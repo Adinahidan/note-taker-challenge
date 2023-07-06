6d2c246900ba83ffaf364d09c47bee5ed5407a7c
Adina <yna_dulci@yahoo.com>
2023-07-07 00:26:56 +0100

    updated server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,23 @@
 const path = require('path');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
 const router = require('express').Router();
+const fs = require('fs');
+const dbPath = path.join(__dirname, '../db/db.json');
 
 router.get('/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, '../db.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to read notes.' });
-    } else {
-      const notes = JSON.parse(data);
-      res.json(notes);
-    }
-  });
+  const notes = JSON.parse(fs.readFileSync(dbPath));
+  res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, '../db.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to read notes.' });
-    } else {
-      const notes = JSON.parse(data);
-      const newNote = req.body;
-      newNote.id = uuidv4();
-      notes.push(newNote);
+  const newNote = req.body;
+  newNote.id = uuidv4();
 
-      fs.writeFile(
-        path.join(__dirname, '../db.json'),
-        JSON.stringify(notes, null, 2),
-        (err) => {
-          if (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Failed to save note.' });
-          } else {
-            res.json(newNote);
-          }
-        }
-      );
-    }
-  });
+  const notes = JSON.parse(fs.readFileSync(dbPath));
+  notes.push(newNote);
+
+  fs.writeFileSync(dbPath, JSON.stringify(notes, null, 2));
+
+  res.json(newNote);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const htmlRoutes = require('./htmlRoutes');
-const apiRoutes = require('./apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
