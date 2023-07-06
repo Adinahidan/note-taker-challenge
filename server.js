853e369b62878bd8ee7f3c8a9a373e8428f8d768
Adina <yna_dulci@yahoo.com>
2023-07-06 22:42:51 +0100

    first commit with folder structure

---
+++ server.js
@@ -0,0 +1,20 @@
+//dependencies
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+//asks express to create a route for every file in the public folder and use the '/' route.
+app.use(express.static("public"));
+//sets up express app to handle the parsing data e.g middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+//starts the server, also known as an app listener
+app.listen(PORT, () => {
+  console.log(`Your server is available at http://localhost:${PORT}`);
+});
